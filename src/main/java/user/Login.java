46e7f0e1aad2226a10aa50fb54ce7717612a10c5
yse-yoo <71994236+yse-yoo@users.noreply.github.com>
2022-07-04 10:31:29 +0900

    show email, password

--- src/main/java/user/Login.java
+++ src/main/java/user/Login.java
@@ -15,6 +15,12 @@ public class Login extends HttpServlet {
 
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		System.out.println("POST!!!");
+		
+		String email = request.getParameter("email");
+		String password = request.getParameter("password");
+		
+		System.out.println(email);
+		System.out.println(password);
 	}
 
 }
